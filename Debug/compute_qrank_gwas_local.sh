94cb276346ab075b3c3464b35314ce6b54ab51fb
daverblair <daverblair@gmail.com>
2020-07-29 16:07:36 -0700

    fixed revealed files

--- Debug/compute_qrank_gwas_local.sh
+++ Debug/compute_qrank_gwas_local.sh
@@ -1,14 +1,14 @@
 
 quantiles="0.99,0.999"
-pheno_file=~/Desktop/MendelianDiseaseProject/Analysis/Section_4/QRankGWAS/QRankDatasets/GBR/OMIM_ICD_108_QRankData.txt
+pheno_file=~/Desktop/MendelianDiseaseProject/Analysis/Section_4/QRankGWAS/QRankDatasets/GBR/OMIM_ICD_86_QRankData.txt
 phenotype_name='Bagged'
 subject_id_col='IID'
-bgen_file_path=/Volumes/WD_Backup/UKBiobank/genetic_data/imputed_genotypes/CHROM_3/ukb_imp_chr3_v3.bgen
-#bgen_file_path=//ukbb/ukb_imp_chr14_v3.bgen
-output_prefix=~/Desktop/MendelianDiseaseProject/QRankGWAS/Debug/tmp_hht_testsnps
+# bgen_file_path=/Volumes/WD_Backup/UKBiobank/genetic_data/imputed_genotypes/CHROM_3/ukb_imp_chr3_v3.bgen
+bgen_file_path=ukbb/ukb_imp_chr14_v3.bgen
+output_prefix=~/Desktop/MendelianDiseaseProject/QRankGWAS/Debug/test
 covariates='sex,age_normalized,array,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10'
 subset=~/Desktop/MendelianDiseaseProject/Data/UKBiobank/filtered_subject_ids_caucasian_rel_excluded.txt
-snps=test_snp_hht.txt
+snps=test_snps.txt
 maf=0.001
 # variant_subset_file=~/Desktop/MendelianDiseaseProject/Analysis/Section_4/BuildVariantTables-4/RandomSubsetSNPs/CHROM_14/RandomSubset.txt
 # variant_subset_file=test_snps.txt
